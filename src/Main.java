de6ffc25491274d69aeb57cd44ceb8e9ae8c9e3f
Krasen Kochev <krasen.kochev.a50@learn.telerikacademy.com>
2023-05-10 15:23:06 +0300

    update Main

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Lets see if any change");
         System.out.println("let me see if this change will be committed");
+        System.out.println("another change");
     }
 }
