b3b44e4e971e872d7b49ad4e5e09b0297bc0e63c
Krasen Kochev <krasen.kochev.a50@learn.telerikacademy.com>
2023-05-10 15:27:34 +0300

    update Main

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Lets see if any change");
         System.out.println("let me see if this change will be committed");
         System.out.println("another change");
+        System.out.println("and another change, because there is no option to uncheck TODO");
     }
 }
